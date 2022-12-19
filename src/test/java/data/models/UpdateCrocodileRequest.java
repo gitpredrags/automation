e04237696e45caeec715ef4de7fb03eb420946eb
gitpredrags <gitpredrags@gmail.com>
2022-12-19 16:34:28 +0100

    commit project

---
+++ src/test/java/data/models/UpdateCrocodileRequest.java
@@ -0,0 +1,56 @@
+package data.models;
+
+import java.io.Serializable;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class UpdateCrocodileRequest implements Serializable
+{
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("sex")
+    @Expose
+    private Object sex;
+    @SerializedName("date_of_birth")
+    @Expose
+    private Object dateOfBirth;
+    private final static long serialVersionUID = -2935748678502921318L;
+
+
+    public UpdateCrocodileRequest() {
+    }
+
+    public UpdateCrocodileRequest(String name, Object sex, Object dateOfBirth) {
+        super();
+        this.name = name;
+        this.sex = sex;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getSex() {
+        return sex;
+    }
+
+    public void setSex(Object sex) {
+        this.sex = sex;
+    }
+
+    public Object getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Object dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+}
---
+++ src/test/java/test/asserts/CrocodileAsserts.java
@@ -0,0 +1,62 @@
+package test.asserts;
+
+import data.models.*;
+import org.testng.asserts.SoftAssert;
+import test.common.TestBase;
+
+public class CrocodileAsserts extends TestBase {
+    public SoftAssert softAssert = new SoftAssert();
+
+    public void assertListOfPublicCrocodiles (GetPublicCrocodileResponse[] getPublicCrocodileResponses){
+        for (int i = 0; i < getPublicCrocodileResponses.length; i++) {
+            softAssert.assertFalse(getPublicCrocodileResponses[i].getName().isEmpty(), "Name is not empty");
+        }
+        softAssert.assertAll();
+    }
+
+    public void assertCrocodileById (GetPublicCrocodileByIdResponse getPublicCrocodileByIdResponse){
+        softAssert.assertEquals(getPublicCrocodileByIdResponse.getId().toString(), publicID , "ID don't match");
+           softAssert.assertAll();
+    }
+
+
+    public void assertListOfPrivateCrocodiles (GetPrivateCrocodileResponse[] getPrivateCrocodileResponses){
+        for (int i = 0; i < getPrivateCrocodileResponses.length; i++) {
+            softAssert.assertFalse(getPrivateCrocodileResponses[i].getName().isEmpty(), "Name is empty");
+            softAssert.assertFalse(getPrivateCrocodileResponses[i].getSex().isEmpty(), "Sex is empty");
+            softAssert.assertFalse(getPrivateCrocodileResponses[i].getDateOfBirth().isEmpty(), "Date of birth is empty");
+        }
+        softAssert.assertAll();
+    }
+
+    public void assertPrivateCrocodileById (GetPrivateCrocodileByIdResponse getPrivateCrocodileByIdResponse){
+        softAssert.assertEquals(getPrivateCrocodileByIdResponse.getId().toString(), privateID, "ID don't match");
+        softAssert.assertAll();
+    }
+
+    public void assertCrocodileNewCrocodile(CreateCrocodileResponse createCrocodileResponse, CreateCrocodileRequest createCrocodileRequest) {
+        softAssert.assertEquals(createCrocodileResponse.getName(), createCrocodileRequest.getName(), "Name didn't match");
+        softAssert.assertEquals(createCrocodileResponse.getSex(), createCrocodileRequest.getSex(), "Sex didn't match");
+        softAssert.assertEquals(createCrocodileResponse.getDateOfBirth(), createCrocodileRequest.getDateOfBirth(), "Date of birth didn't match");
+        softAssert.assertAll();
+    }
+
+    public void assertUpdateCrocodile (UpdateCrocodileResponse updateCrocodileResponse, UpdateCrocodileRequest updateCrocodileRequest){
+        softAssert.assertEquals(updateCrocodileResponse.getName(), updateCrocodileRequest.getName(), "Name didn't match");
+        softAssert.assertEquals(updateCrocodileResponse.getSex(), updateCrocodileRequest.getSex(), "Sex didn't match");
+        softAssert.assertEquals(updateCrocodileResponse.getDateOfBirth(), updateCrocodileRequest.getDateOfBirth(), "Date if birth didn't match");
+        softAssert.assertAll();
+    }
+
+    public void assertUpdateCrocodileName (UpdateNameFieldResponse updateNameFieldResponse, UpdateNameFieldRequest updateNameFieldRequest){
+        softAssert.assertEquals(updateNameFieldRequest.getName(), updateNameFieldResponse.getName(), "Name is not updated");
+        softAssert.assertAll();
+    }
+
+    public void assertDeleteCrocodile (GetPrivateCrocodileResponse[] getPrivateCrocodileResponse){
+        for (int i = 0; i<getPrivateCrocodileResponse.length; i++){
+            softAssert.assertNotEquals(getPrivateCrocodileResponse[i].getId().toString(), "12203160", "Crocodile is not deleted");
+        }
+        softAssert.assertAll();
+    }
+}
